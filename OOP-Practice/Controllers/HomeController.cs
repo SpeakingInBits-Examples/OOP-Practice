0579b04384fa8d66cfad56dc8385439ca8a53ff7
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-10-20 19:41:40 -0700

    Add project files.

---
+++ OOP-Practice/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OOP_Practice.Models;
+
+namespace OOP_Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OOP-Practice/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OOP_Practice.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    [DataType(DataType.Currency)]
+    public decimal Price { get; set; }
+}
