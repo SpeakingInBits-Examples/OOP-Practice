921b2bcf0d59538c6a068f9f03f99daa39b6d137
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-10-20 19:49:40 -0700

    Add EF Core DbContext and configure SQL Server support
    Added `AppDbContext` to manage `Product` entities and configured it with SQL Server in `Program.cs`. Updated `OOP-Practice.csproj` to include necessary EF Core and scaffolding packages. Added a connection string for `AppDbContext` in `appsettings.json` to use a local SQL Server database.

---
+++ OOP-Practice/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OOP_Practice.Models;
+
+namespace OOP_Practice.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext (DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Product { get; set; }
+}
